fb869b2cfd48cb9e70a6d5a29fbec4f9be6c6747
AntLacerda <lacerdaneto99@hotmail.com>
2023-03-23 16:39:30 -0300

    Inicializando corpo do projeto

--- arqdecor/src/App.js
+++ arqdecor/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Header from './Componentes/Header'
+import Header from './Componentes/Header';
+import Corpo from './Componentes/Corpo';
 
 function App() {
   return (
     <>
       <Header/>
+      <Corpo/>
     </>
   )
 }
---
+++ arqdecor/src/Componentes/Corpo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Corpo() {
+    return (
+        <main>
+            <div className='box'>
+
+            </div>
+        </main>
+    )
+}
+
+export default Corpo;
