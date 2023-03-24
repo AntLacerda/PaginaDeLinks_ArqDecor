32b508453bf7e7f85c1c8941bd11115e99cbb367
AntLacerda <lacerdaneto99@hotmail.com>
2023-03-23 21:43:28 -0300

    Desenvolvendo logotipo e titulo - corpo do projeto

--- arqdecor/src/Componentes/Corpo.js
+++ arqdecor/src/Componentes/Corpo.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import '../CSS/Box.css';
+import Logotipo from '../img/logotipo.jpg'
 
 function Corpo() {
     return (
         <main>
             <div className='box'>
-
+                <div className='div_img'>
+                    <img src={Logotipo}/>
+                </div>
+                <div className='div_texto'>
+                    <h2>Escritório de Arquitetura e Urbanismo</h2>
+                    <p>Construindo novos conceitos de arquitetura à decoração.</p>
+                </div>
             </div>
         </main>
     )
